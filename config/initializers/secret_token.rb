f90f4d42989c5a155b4b8d7424e2951c5421997a
Blaine Pace <blainepace@gmail.com>
2012-09-16 19:57:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KinozeitServer::Application.config.secret_token = 'd6d806c922142e12bc14cb301adfe0ab354ab15f6f8191a00513f80756bf12fa7542dba6f55d7515c2dab8105ad4f2919f2eeb89478c6868482e7a1a57170b49'
